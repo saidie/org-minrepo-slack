fe64374676bd2fb5feffff720b06f6f155345059
Hiroshi Saito <saidie@saidie.info>
2016-02-20 16:20:18 +0900

    Add comments

--- org-minrepo-slack.el
+++ org-minrepo-slack.el
@@ -1,3 +1,26 @@
+;;; org-minrepo-slack.el --- Report current clocking-in task to Slack
+
+;; Copyright (C) 2016 by Hiroshi Saito
+
+;; Author: Hiroshi Saito <monodie@gmail.com>
+;; URL: https://github.com/saidie/org-minrepo-slack
+;; Version: 0.9.0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (require 'json)
 
 (defvar oms/slack-webhook-url nil)
@@ -32,3 +55,5 @@
       (message "will notify to Slack")
       (run-at-time oms/notification-buffer nil
                    'oms/send-notification current-task))))
+
+;;; org-minrepo-slack.el ends here
